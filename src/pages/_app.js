d2a5c300f974341fdf30c83ab13b1fe20e884d4e
pyaephyothant <pyaephyothant1998@gmail.com>
2021-10-03 13:35:49 +0630

    update meta title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,14 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Theme>
+        <Head>
+          <title>Pyae Phyo Thant | Info</title>
+          <meta
+            property="og:title"
+            content="pyae phyo thant personal information"
+            key="title"
+          />
+        </Head>
         <Component {...pageProps} />
       </Theme>
     </>
