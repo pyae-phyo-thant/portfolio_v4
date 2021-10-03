df486099743caaf71e7b76d4f8b45a00547eecf8
pyaephyothant <pyaephyothant1998@gmail.com>
2021-10-03 14:40:18 +0630

    fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/pages/**/*.js", "./components/**/*.js"],
+  purge: ["./src/pages/*.js", "./src/components/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
