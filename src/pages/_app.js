5cda1e844f0506562de69338429255e8c646c5ce
pyaephyothant <pyaephyothant1998@gmail.com>
2021-10-10 12:54:26 +0630

    adding google analytic

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,22 +3,38 @@ import "../styles/globals.css";
 import Head from "next/head";
 import { AnimatePresence } from "framer-motion";
 import { UserProvider } from "@auth0/nextjs-auth0";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import * as ga from "../../lib/ga";
 
 export default function App({ Component, pageProps, router }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
       <Theme>
         <Head>
           <title>Pyae Phyo Thant | Info</title>
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=UA-209807215-1"
-          ></script>
-          <script>
-            window.dataLayer = window.dataLayer || []; function gtag()
-            {dataLayer.push(arguments)}
-            gtag('js', new Date()); gtag('config', 'UA-209807215-1');
-          </script>
+          <meta
+            property="og:title"
+            content="pyae phyo thant personal information"
+            key="title"
+          />
         </Head>
         <AnimatePresence>
           <UserProvider>
